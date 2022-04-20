8ac1acb767e5b691258f76c2be30f4efeb1a4f93
pawel.olas <pawel.olas@findesign.com.au>
2022-04-20 10:06:37 +1000

    proof of concept working

---
+++ src/main/kotlin/com/github/p0las/dividers/actions/SingleLineDividerAction.kt
@@ -0,0 +1,81 @@
+package com.github.p0las.dividers.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.util.TextRange
+import java.io.File
+
+/**
+ * Menu action to replace a selection of characters with a fixed string.
+ *
+ * @see AnAction
+ */
+class SingleLineDividerAction : AnAction() {
+    /**
+     * Replaces the run of text selected by the primary caret with a fixed string.
+     *
+     * @param e Event related to this action
+     */
+    override fun actionPerformed(e: AnActionEvent) {
+        // Get all the required data from data keys
+        // Editor and Project were verified in update(), so they are not null.
+        val editor = e.getRequiredData(CommonDataKeys.EDITOR)
+        val project = e.getRequiredData(CommonDataKeys.PROJECT)
+        val document = editor.document
+
+        val primaryCaret = editor.caretModel.primaryCaret
+        primaryCaret.selectLineAtCaret()
+        val start = primaryCaret.selectionStart
+        val end = primaryCaret.selectionEnd
+
+        File("C:/temp/kotlin.log").writeText(editor.editorKind.toString())
+
+        val range = TextRange(start,end)
+        val text = editor.document.getText(range)
+        val textTrimmed = text.trim()
+
+        val lineLength:Int = 120
+        val dividerPattern = "-"
+        val comment = "#"
+
+        var margin:Int = 0
+        for (i in text) {
+            if (i == ' '){
+                margin++
+            }
+            else{
+                break
+            }
+        }
+        val marginOffset = " ".repeat(margin)
+
+        val length = (lineLength - textTrimmed.length - margin - 4)/2
+        val line = dividerPattern.repeat(length/dividerPattern.length)
+
+        WriteCommandAction.runWriteCommandAction(project
+        ) { document.replaceString(start, end, "$marginOffset$comment$line<  $textTrimmed  >$line\n") }
+        // De-select the text range that was just replaced
+        primaryCaret.removeSelection()
+    }
+
+    /**
+     * Sets visibility and enables this action menu item if:
+     *
+     *  * a project is open
+     *  * an editor is active
+     *  * some characters are selected
+     *
+     *
+     * @param e Event related to this action
+     */
+    override fun update(e: AnActionEvent) {
+        // Get required data keys
+        val project = e.project
+        val editor = e.getData(CommonDataKeys.EDITOR)
+        // Set visibility and enable only in case of existing project and editor
+
+        e.presentation.isEnabledAndVisible = project != null && editor != null // && editor.selectionModel.hasSelection()
+    }
+}
